40f2fa3df67dba82f4bf5aa8ec459ab0dac12cd9
Adam Weiss <113201202+adamweiss22@users.noreply.github.com>
2024-09-06 16:03:46 -0500

    Update DCF_Calculator.py

--- DCF_Calculator.py
+++ DCF_Calculator.py
@@ -1,28 +1,24 @@
 import pandas as pd
-import sys
 import warnings
+import statistics
 warnings.filterwarnings('ignore', category=FutureWarning)
 
 try:
     import yfinance as yf
 except ImportError:
     print("Error: The 'yfinance' package is not installed.")
-    print("Please install it by running: pip install yfinance")
-    sys.exit(1)
+    raise
 
-if len(sys.argv) != 4:
-    print("Usage: python DCF_Calculator.py <ticker> <years> <case>")
-    sys.exit(1)
-# Handling incorrect or missing inputs
+
 def is_valid_ticker(ticker):
     try:
         company = yf.Ticker(ticker)
-        company.info  # This will raise an exception if the ticker is invalid
+        company.info  
         return company
     except Exception:
         return None
 
-# Function to validate the years input
+
 def is_valid_years(years):
     try:
         years = int(years)
@@ -33,7 +29,7 @@ def is_valid_years(years):
     except ValueError:
         return None
 
-# Function to validate the case input
+
 def is_valid_case(case):
     if case in ['1', '2', '3']:
         return int(case)
@@ -46,7 +42,7 @@ def calculate_historical_growth_rate(financials):
     avg_growth_rate = growth_rates.mean()
     best_case = avg_growth_rate - growth_rates.std()
     worst_case = avg_growth_rate + growth_rates.std()
-    return 1+(-1*avg_growth_rate), 1+(-1*best_case), 1+(-1*worst_case)
+    return 1 + (-1 * avg_growth_rate), 1 + (-1 * best_case), 1 + (-1 * worst_case)
 
 def calculate_wacc(company):
     # Get the beta value
@@ -87,29 +83,21 @@ def calculate_wacc(company):
     
     return wacc
 
-def main():
-    # Validate stock ticker
-    company = is_valid_ticker(sys.argv[1])
+def main(ticker, years, case):
+    company = is_valid_ticker(ticker)
     if not company:
         print("Error: Invalid stock ticker.")
         return
 
-    # Validate years input
-    years = is_valid_years(sys.argv[2])
+    years = is_valid_years(years)
     if not years:
         print("Error: Years must be a positive integer.")
         return
 
-    # Validate case input
-    case = is_valid_case(sys.argv[3])
+    case = is_valid_case(case)
     if case is None:
         print("Error: Case must be 1, 2, or 3.")
         return
-
-
-
-
-
 
     base, bull, bear = calculate_historical_growth_rate(company)
 
@@ -120,23 +108,16 @@ def main():
     else:
         case = bear
 
-    
     df1 = company.income_stmt[company.income_stmt.columns[0:3]].loc[['Total Revenue', 'Operating Expense', 'Reconciled Depreciation', 'Interest Expense', 'Tax Rate For Calcs']]
     df2 = company.cash_flow[company.cash_flow.columns[0:3]].loc[['Operating Cash Flow', 'Capital Expenditure']]
     df3 = company.balance_sheet[company.balance_sheet.columns[0:3]].loc[['Working Capital']]
     merged_df = pd.concat([df1, df2, df3])
     merged_df.loc['FCF'] = merged_df.loc['Operating Cash Flow'] + merged_df.loc['Capital Expenditure']
 
-
-
-
-    # TODO: add option to choose custom growth rate
-    
-
     # Revenue Projections
     future_revenue = []
-    for i in range(1,years+1):
-        future_revenue.append(merged_df.loc['Total Revenue'][merged_df.columns[0]]*case**i)
+    for i in range(1, years + 1):
+        future_revenue.append(merged_df.loc['Total Revenue'][merged_df.columns[0]] * case ** i)
 
     # Operational Expenses as a Percent of Revenue
     future_opex = []
@@ -148,10 +129,10 @@ def main():
     wc_as_perc_of_rev = (merged_df.loc['Working Capital'] / merged_df.loc['Total Revenue']).mean()
     future_working_cap = [merged_df.loc['Working Capital'][-1]]
     for i in range(years):
-        future_working_cap.append(future_revenue[i]*wc_as_perc_of_rev)
+        future_working_cap.append(future_revenue[i] * wc_as_perc_of_rev)
     future_change_working_cap = []
     for i in range(len(future_working_cap) - 1):
-        future_change_working_cap.append(future_working_cap[i+1] - future_working_cap[i])
+        future_change_working_cap.append(future_working_cap[i + 1] - future_working_cap[i])
 
     # Capital Expenditures Projections
     future_capex = []
@@ -165,12 +146,11 @@ def main():
     for i in range(years):
         future_dep.append(avg_depreciation)
 
-
     # FCF Calculations
     future_fcf = []
     for i in range(years):
         ebit = future_revenue[i] - future_opex[i]
-        FCF = (ebit * (1- merged_df.loc['Tax Rate For Calcs'][0])) + future_dep[i] - future_change_working_cap[i] + future_capex[i]
+        FCF = (ebit * (1 - merged_df.loc['Tax Rate For Calcs'][0])) + future_dep[i] - future_change_working_cap[i] + future_capex[i]
         future_fcf.append(FCF)
         
     wacc = calculate_wacc(company)
@@ -178,12 +158,9 @@ def main():
     g = 0.03
     for i in range(years):
         pv_fcf.append(future_fcf[i] / ((1 + wacc) ** (i + 1)))
-    pv_terminal_value = ((future_fcf[-1] * (1+g)) / (wacc-g)) / ((1+wacc)**years)
+    pv_terminal_value = ((future_fcf[-1] * (1 + g)) / (wacc - g)) / ((1 + wacc) ** years)
     equity_value = sum(pv_fcf) + pv_terminal_value - company.balance_sheet.loc['Net Debt'][0]
     estimated_price = equity_value / company.info.get('sharesOutstanding')
-    print(f"Estimated: {estimated_price}")
-    print(f"Day Low: {company.info.get('dayLow')}")
-    print(f"Day High: {company.info.get('dayHigh')}")
+    
+    return estimated_price 
 
-if __name__ == "__main__":
-    main()
